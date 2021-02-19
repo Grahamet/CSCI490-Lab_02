fe883ab3bc42669cd2670caed4a77bf7cc201fcd
grahamet <grahamet@g.cofc.edu>
2021-02-18 19:50:58 -0500

    fixed image sizes

--- app/src/main/java/com/example/intentdemo/secondActivity.java
+++ app/src/main/java/com/example/intentdemo/secondActivity.java
@@ -1,11 +1,8 @@
 package com.example.intentdemo;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
