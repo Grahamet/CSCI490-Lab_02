90668dcac8951eb09aead4438d9350d6724a103c
grahamet <grahamet@g.cofc.edu>
2021-02-18 19:43:25 -0500

    part 2

--- app/src/main/java/com/example/intentdemo/MainActivity.java
+++ app/src/main/java/com/example/intentdemo/MainActivity.java
@@ -1,41 +1,51 @@
 package com.example.intentdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final int REQUEST_CODE =1;
-
-    private Button button:
-    private Intent i;
-    private EditText editText;
+    public static final int BACKGROUND_CODE = 1;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.plain_text_input);
-
         button = findViewById(R.id.button);
-        i = new Intent(this, secondActivity.class);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String s = editText.getText().toString();
-                i.putExtra("testString",s);
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "I called Intent", Toast.LENGTH_SHORT).show();
 
-                startActivity(i);
-                    startActivityForResult(i,REQUEST_CODE);
+                Intent intent = new Intent(getApplicationContext(), secondActivity.class);
+
+                // This is to launch another Activity with expectation that it will return
+                startActivityForResult(intent, BACKGROUND_CODE);
             }
         });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == BACKGROUND_CODE && resultCode == Activity.RESULT_OK) {
+            Bundle extras = data.getExtras();
+            if (extras != null) {
+                int imageID = extras.getInt("imageID");
+                ConstraintLayout currentLayout = findViewById(R.id.main_layout);
+                currentLayout.setBackground(getDrawable(imageID));
+
+            }
+        }
     }
 }
+
--- app/src/main/java/com/example/intentdemo/secondActivity.java
+++ app/src/main/java/com/example/intentdemo/secondActivity.java
@@ -1,37 +1,60 @@
 package com.example.intentdemo;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class secondActivity extends AppCompatActivity {
 
-    TextView textview2;
+    private ImageView moon;
+    private ImageView waterfallImg;
+
+    private int imageID;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Intent i = getIntent();
-        String s = i.getExtras().getString("testString");
+        moon = findViewById(R.id.supermoon);
+        waterfallImg = findViewById(R.id.waterfall);
 
-        textview2 = findViewById(R.id.textView2);
+        moon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "I clicked supermoon", Toast.LENGTH_SHORT).show();
+                imageID = R.drawable.supermoon;
+                finish();
 
-        textview2.setText(s);
+            }
+        });
 
-        finish();
+        waterfallImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "I clicked waterfall", Toast.LENGTH_SHORT).show();
+                imageID = R.drawable.waterfall;
+                finish();
+            }
+        });
+
     }
 
-//    @Override
-//    public void finish(){
-//        Intent intent = new Intent();
-//        intent.putExtra("imageID", imageID);
-//
-//        setResult(RESULT_OK, intent);
-//        super.finish();
-//    }
+    // This is the response to ActivityForResult call
+    @Override
+    public void finish() {
+        Intent intent = new Intent();
+        intent.putExtra("imageID", imageID);
+
+        setResult(RESULT_OK, intent);
+        super.finish();
+    }
 
 }
